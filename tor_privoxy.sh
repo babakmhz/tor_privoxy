1f5ea747697ee5d7de9e399f524a09e5bc48c92e
babakmhz <babakmhz96@gmail.com>
2019-04-23 15:26:58 +0430

    help created

--- tor_privoxy.sh
+++ tor_privoxy.sh
@@ -1,43 +1,71 @@
-#!/bin/zsh
+#!/bin/bash
 
-# echo "$(whoami)"
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
-print "welcome to my shell script!!!\nconfiguring tor with privoxy\nafter configuring please set your proxy system to 127.0.0.1:8118 / socks proxy: 127.0.0.1:9050" 
-input=$1    
+
+input=$1
 
 copy_config(){
-    echo 'configuring privoxy config file... '
+    echo 'configuring privoxy config file... /n ========================'
     sudo cp /etc/privoxy/config /etc/privoxy/config.sample
     echo 'done!'
 }
 disable(){
     print 'configuring....'
-    echo 'after this oprations please set your system wide proxy to NONE'
+    echo 'you can set your system wide proxy to NONE!'
     sudo cat /etc/privoxy/config.sample > /etc/privoxy/config
     systemctl stop tor.service ; systemctl stop privoxy.service
     echo 'done!'
 }
 enable(){
-    echo 'after this oprations you need to set your system wide proxy to 127.0.0.1:8118 / socks proxy: 127.0.0.1:9050 '
-    echo 'forward-socks5 / localhost:9050 .' >>  /etc/privoxy/config 
+    echo 'you need to set your system wide proxy to 127.0.0.1:8118 / socks proxy: 127.0.0.1:9050!'
+    echo 'forward-socks5 / localhost:9050 .' >>  /etc/privoxy/config
     echo 'forward-socks4 / localhost:9050 .' >>  /etc/privoxy/config
     echo 'forward-socks4a / localhost:9050 .' >>  /etc/privoxy/config
     systemctl start tor.service ; systemctl start privoxy.service
+    echo ''
     echo 'enebled'
 }
-ls /etc/privoxy/config.sample || copy_config
 
- 
+# restart(){
+    
+# }
+
+help(){
+    echo 'INSTALLATION:'
+    echo '    tor_privoxy.sh --install'
+    echo 'CONFIGURE:'
+    echo '    tor_privoxy.sh --configure <distro_name> <bash_name>'
+    echo 'Supported Distro Names:['arch','debian']'
+    echo 'Supported Bash Names:['bash','zsh']'
+
+}
+
+# install(){
+    
+# }
+
+
+# terminal_config(){
+    
+# }
+
+echo 'tor privoxy runner V1.0'
+echo '========================'
+
 if [ !$ls_result = "/etc/privoxy/config.sample" ]; then
     echo 'not found!!!'
 fi
 
-if [ $input = "d" ]
- then
-    disable
-elif [ $input = "e" ]
+if [ -z "$input" ]
+then
+    help
+elif [ $input = "-e" ]
 then
     enable
+    
+elif [ $input = "-d" ]
+then
+    disable
 fi
 
 
